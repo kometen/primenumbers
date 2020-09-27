8dfd915d7c158d7090b45e68dbf1ae15f5224e75
Claus Guttesen <kometen@gmail.com>
2020-09-27 17:45:11 +0200

    Added command line arguments.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,32 @@
+extern crate clap;
+
+use clap::{Arg, App};
+
 fn main() {
+    // Command line parameters.
+    let matches = App::new("primenumbers")
+    .version("0.1")
+    .about("calculate primenumbers")
+    .author("Claus Guttesen")
+    .arg(Arg::with_name("limit")
+        .help("upper limit")
+        .required(true)
+        .takes_value(true)
+        .short("l")
+        .long("limit")
+        .multiple(true)
+    )
+    .get_matches();
+
+    let limit = match matches.value_of("limit").unwrap().trim().parse::<usize>() {
+        Ok(limit) => limit,
+        Err(_e) => { 0 }
+    };
+
     let mut primes: Vec<usize> = vec![2];
     let mut is_prime: bool = false;
 
-    for x in 3..200000 {
+    for x in 3..limit {
         for &prime in &primes {
             // Assume it's a prime.
             is_prime = true;
